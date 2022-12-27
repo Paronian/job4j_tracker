4c0da058f791b5abc6c7e44c38cb1a3a891d3959
Bagrat <bagiration@yandex.ru>
2022-12-27 19:09:38 +0300

    1.3. Вызов метода с аргументами. [#166086 #375109] (избавился от вложенности)

--- src/main/java/ru/job4j/oop/Ball.java
+++ src/main/java/ru/job4j/oop/Ball.java
@@ -1,7 +1,7 @@
 package ru.job4j.oop;
 
 public class Ball {
-    public void tryRun(boolean condition){
+    public void tryRun(boolean condition) {
         String result = condition ? "Колобок съеден" : " Колобок сбежал";
         System.out.println(result);
     }
