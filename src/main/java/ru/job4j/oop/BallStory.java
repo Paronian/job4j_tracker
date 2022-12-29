6768ec8948241c701513d68406dee9345255be71
Bagrat <bagiration@yandex.ru>
2022-12-29 13:28:37 +0300

    1.6. Взаимодействие объектов [#166087 #375201]

--- src/main/java/ru/job4j/oop/BallStory.java
+++ src/main/java/ru/job4j/oop/BallStory.java
@@ -1,10 +1,10 @@
 package ru.job4j.oop;
 
 public class BallStory {
-Ball ball = new Ball();
-Fox fox = new Fox();
-Hare hare = new Hare();
-Wolf wolf = new Wolf();
+    Ball ball = new Ball();
+    Fox fox = new Fox();
+    Hare hare = new Hare();
+    Wolf wolf = new Wolf();
 }
 
 
