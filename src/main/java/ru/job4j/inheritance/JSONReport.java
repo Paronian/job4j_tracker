57a763a9d7a3c1e51999efc1fe38e5fe715a05b4
Bagrat <bagiration@yandex.ru>
2023-01-02 01:47:20 +0300

    4. Переопределение метода [#174890]

---
+++ src/main/java/ru/job4j/inheritance/JSONReport.java
@@ -0,0 +1,9 @@
+package ru.job4j.inheritance;
+
+public class JSONReport extends TextReport {
+    public String generate(String name, String bode) {
+        String ln = System.lineSeparator();
+        return "{" + ln + "\t\"name\" : " + "\"" + name + "\"" + ","
+                + ln + "\t\"body\" : " + "\"" + bode + "\"" + ln + "}";
+    }
+}
