6ae5c124ca06a042bb12a61e8c6818576ac2ef75
Bagrat <bagiration@yandex.ru>
2023-01-03 23:24:22 +0300

    0. Что такое инкапсуляция. [#3818]

--- src/main/java/ru/job4j/tracker/UsagePackageDate.java
+++ src/main/java/ru/job4j/tracker/UsagePackageDate.java
@@ -9,7 +9,7 @@ public class UsagePackageDate {
         System.out.println("Текущие дата и время до форматирования: " + currentDateTime);
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
         String currentDateTimeFormat = currentDateTime.format(formatter);
-        String padla = LocalDateTime.now().format(formatter);
+        String padla = LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss"));
         System.out.println("Текущие дата и время после форматирования: " + currentDateTimeFormat);
         System.out.println("Текущие дата и время после форматирования: " + padla);
     }
