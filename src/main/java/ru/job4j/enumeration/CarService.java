6aeaeacdc9da648922b6e3b62e7bf5633a323194
Bagrat <bagiration@yandex.ru>
2023-01-06 23:33:21 +0300

    15. Аргументы переменной длины (varargs) [#504977 #378348]

---
+++ src/main/java/ru/job4j/enumeration/CarService.java
@@ -0,0 +1,30 @@
+package ru.job4j.enumeration;
+
+public class CarService {
+    public static void main(String[] args) {
+        Order order = new Order(1, "Mercedes-Benz GLS", Status.IN_WORK);
+        System.out.println("Заказ-наряд №" + order.getNumber() + " на автомобиль " + order.getCar()
+                + ", статус ремонта: " + order.getStatus().getMessage());
+
+        Status[] statuses = Status.values();
+        for (Status s : statuses) {
+            System.out.println("Название статуса: " + s.name() + ", Порядковый номер статуса: " + s.ordinal());
+        }
+
+        switch (order.getStatus()) {
+            case ACCEPTED:
+                System.out.println("Статус: Автомобиль принят на СТО");
+                break;
+            case IN_WORK:
+                System.out.println("Статус: Автомобиль в работе");
+                break;
+            case WAITING:
+                System.out.println("Статус: Автомобиль ожидает запчасти");
+                break;
+            case FINISHED:
+                System.out.println("Статус: Все работы завершены");
+                break;
+            default:
+        }
+    }
+}
