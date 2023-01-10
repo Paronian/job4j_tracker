b8793d8b11e886e320e3d4a9504198b9a9c68e44
Bagrat <bagiration@yandex.ru>
2023-01-10 22:55:10 +0300

    4.2. Интерфейс Input [#181092]

---
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -0,0 +1,18 @@
+package ru.job4j.poly;
+
+public class Bus implements Transport {
+    @Override
+    public void drive() {
+        System.out.println("Вождение автобуса по маршруту");
+    }
+
+    @Override
+    public void passengers(int numbers) {
+        System.out.println("В автобусе находится " + numbers + " пассажиров");
+    }
+
+    @Override
+    public double refuel(double volume) {
+        return volume * 15;
+    }
+}
