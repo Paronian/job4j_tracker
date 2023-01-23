207e7ecd6acbd8bccda5d854ffb215b4c38de504
Bagrat <bagiration@yandex.ru>
2023-01-24 00:07:56 +0300

    https://job4j.ru/profile/exercise/24/task/955/383650

--- src/main/java/ru/job4j/early/PasswordValidator.java
+++ src/main/java/ru/job4j/early/PasswordValidator.java
@@ -1,89 +1,65 @@
 package ru.job4j.early;
 
 public class PasswordValidator {
-    static boolean containsUpperCaseLetter(String password) {
-        boolean rsl = false;
-        for (int i = 0; i < array(password).length; i++) {
-            if (Character.isUpperCase(array(password)[i])) {
-                rsl = true;
-                break;
-            }
-        }
-        return rsl;
-    }
-
-    static boolean containsLowerCase(String password) {
-        boolean rsl = false;
-        for (int i = 0; i < array(password).length; i++) {
-            if (Character.isLowerCase(array(password)[i])) {
-                rsl = true;
-                break;
-            }
-        }
-        return rsl;
-    }
-
-    static boolean containsFigure(String password) {
-        boolean rsl = false;
-        for (int i = 0; i < array(password).length; i++) {
-            if (Character.isDigit(array(password)[i])) {
-                rsl = true;
-                break;
-            }
-        }
-        return rsl;
-    }
-
-    static boolean containSpecialSymbol(String password) {
-        int code = -1;
-        boolean rsl = false;
-        for (int i = 0; i < array(password).length; i++) {
-            code = Character.codePointAt(array(password), i);
-            if (!Character.isAlphabetic(code)
-                    && !Character.isDigit(array(password)[i])) {
-                rsl = true;
-                break;
-            }
-        }
-        return rsl;
-    }
-
     static boolean containSubstringSubString(String password) {
+        boolean rsl = false;
+        String[] notValidString = {"qwerty", "12345", "password", "admin", "user"};
         password = password.toLowerCase();
-        return password.contains("qwerty")
-                || password.contains("12345")
-                || password.contains("password")
-                || password.contains("admin")
-                || password.contains("user");
-    }
-
-    static char[] array(String password) {
-        return password.toCharArray();
+        for (int i = 0; i < notValidString.length; i++) {
+            if (password.contains(notValidString[i])) {
+                rsl = true;
+                break;
+            }
+        }
+        return rsl;
     }
 
     public static String validate(String password) {
+        boolean rslUpperCase = false;
+        boolean rslLowerCase = false;
+        boolean rslDigit = false;
+        boolean rslAlphabetic = false;
+        int code = -1;
         if (password == null) {
             throw new IllegalArgumentException("Password can't be null");
         }
-        int arrayLength = array(password).length;
+        char[] array = password.toCharArray();
+        int arrayLength = password.toCharArray().length;
+
         if (arrayLength <= 8 || arrayLength >= 32) {
             throw new IllegalArgumentException("Password should be length [8, 32]");
         }
-        if (!containsUpperCaseLetter(password)) {
-            throw new IllegalArgumentException("Password should contain at least one uppercase letter");
-        }
-        if (!containsLowerCase(password)) {
-            throw new IllegalArgumentException("Password should contain at least one lowercase letter");
-        }
-        if (!containsFigure(password)) {
-            throw new IllegalArgumentException("Password should contain at least one figure");
-        }
-
-        if (!containSpecialSymbol(password)) {
-            throw new IllegalArgumentException("Password should contain at least one special symbol");
-        }
         if (containSubstringSubString(password)) {
             throw new IllegalArgumentException("Password shouldn't contain substrings: qwerty, 12345, password, admin, user");
+        }
+
+        for (int i = 0; i < array.length; i++) {
+            code = Character.codePointAt(array, i);
+            if (Character.isUpperCase(array[i])) {
+                rslUpperCase = true;
+            }
+            if (Character.isLowerCase(array[i])) {
+                rslLowerCase = true;
+            }
+            if (Character.isDigit(array[i])) {
+                rslDigit = true;
+            }
+            if (!Character.isAlphabetic(code)
+                    && !Character.isDigit(array[i])) {
+                rslAlphabetic = true;
+            }
+        }
+        if (!rslUpperCase) {
+            throw new IllegalArgumentException("Password should contain at least one uppercase letter");
+        }
+        if (!rslLowerCase) {
+            throw new IllegalArgumentException("Password should contain at least one lowercase letter");
+        }
+        if (!rslDigit) {
+            throw new IllegalArgumentException("Password should contain at least one figure");
+        }
+        if (!rslAlphabetic) {
+            throw new IllegalArgumentException("Password should contain at least one special symbol");
         }
         return password;
     }
