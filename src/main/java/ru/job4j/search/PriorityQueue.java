a6f7895eae99e26620dbcd5143263688d15dc3c9
Bagrat <bagiration@yandex.ru>
2024-09-13 21:45:17 +0300

    5. Очередь с приоритетом на LinkedList [#41670]

--- src/main/java/ru/job4j/search/PriorityQueue.java
+++ src/main/java/ru/job4j/search/PriorityQueue.java
@@ -15,9 +15,11 @@ public class PriorityQueue {
 
     public void put(Task task) {
         int index = 0;
-        /*for (Task element : tasks) {
-            if (element.getPriority() >)
-        }*/
+        for (Task element : tasks) {
+            if (task.getPriority() < element.getPriority()) {
+                index = element.getPriority();
+            }
+        }
         this.tasks.add(index, task);
     }
 
