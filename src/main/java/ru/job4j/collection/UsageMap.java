2cd5b5d28ba7710bdb65161686a75bb7b9d548b2
Bagrat <bagiration@yandex.ru>
2024-10-02 20:24:04 +0300

    0. Map, HashMap [#212652 #515020]

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -8,6 +8,10 @@ public class UsageMap {
         HashMap<String, String> map = new HashMap<>();
         map.put("bagrat@mail.ru", "Юлов Бор Васильевич");
         map.put("yusupov@yandex.ru", "Юсупов Илья Васильевич");
+        map.put("yusupov@yandex.ru", "Юсупов Илья Васильевич");
+        map.put("bagrat@mail.ru", "Паронян Баграт Ашотович");
+        map.put("yynn@mail.ru", "Паронян Баграт Ашотович");
+        map.put("anna@yandex.ru", "Юлов Бор Васильевич");
         for (String key: map.keySet()) {
             String value = map.get(key);
             System.out.println(key + " = " + value);
