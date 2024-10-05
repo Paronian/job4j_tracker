0e869ad9aff64aafb02e61f16c6f68991d5ca77b
Bagrat <bagiration@yandex.ru>
2024-10-05 11:46:39 +0300

    3. Банковские переводы [#10038 #515466]

--- src/main/java/ru/job4j/bank/BankService.java
+++ src/main/java/ru/job4j/bank/BankService.java
@@ -16,32 +16,21 @@ public class BankService {
     }
 
     public void deleteUser(String passport) {
-        User checkUser = findByPassport(passport);
-        if (checkUser != null) {
-            users.remove(checkUser);
-        }
+        users.remove(new User(passport, ""));
     }
 
     public void addAccount(String passport, Account account) {
         User user = findByPassport(passport);
-        if (user == null) {
-            return;
+        if (user != null
+                && !users.get(user).contains(account)) {
+            users.get(user).add(account);
         }
-        List<Account> list = users.get(user);
-        for (Account el : list) {
-            String temp = el.getRequisite();
-            if (account.getRequisite().equals(temp)) {
-                return;
-            }
-        }
-        list.add(account);
     }
 
     public User findByPassport(String passport) {
-        for (Map.Entry<User, List<Account>> entry : users.entrySet()) {
-            String key = entry.getKey().getPassport();
-            if (key.equals(passport)) {
-                return entry.getKey();
+        for (User key : users.keySet()) {
+            if (key.getPassport().equals(passport)) {
+                return key;
             }
         }
         return null;
@@ -49,13 +38,12 @@ public class BankService {
 
     public Account findByRequisite(String passport, String requisite) {
         User user = findByPassport(passport);
-        if (user == null) {
-            return null;
-        }
-        for (Account el : users.get(user)) {
-            String temp = el.getRequisite();
-            if (requisite.equals(temp)) {
-                return el;
+        if (user != null) {
+            for (Account el : users.get(user)) {
+                String temp = el.getRequisite();
+                if (requisite.equals(temp)) {
+                    return el;
+                }
             }
         }
         return null;
@@ -64,22 +52,17 @@ public class BankService {
     public boolean transferMoney(String sourcePassport, String sourceRequisite,
                                  String destinationPassport, String destinationRequisite,
                                  double amount) {
+        boolean result = false;
         Account sourceAccount = findByRequisite(sourcePassport, sourceRequisite);
         Account destinationAccount = findByRequisite(destinationPassport, destinationRequisite);
-
-        if (destinationAccount == null) {
-            return false;
+        if (sourceAccount != null
+                && destinationAccount != null
+                && sourceAccount.getBalance() - amount >= 0) {
+            sourceAccount.setBalance(sourceAccount.getBalance() - amount);
+            destinationAccount.setBalance(destinationAccount.getBalance() + amount);
+            result = true;
         }
-        if (sourceAccount == null) {
-            return false;
-        }
-        double sourceAmount = sourceAccount.getBalance();
-        if (sourceAmount - amount < 0) {
-            return false;
-        }
-        sourceAccount.setBalance(sourceAccount.getBalance() - amount);
-        destinationAccount.setBalance(destinationAccount.getBalance() + amount);
-        return true;
+        return result;
     }
 
     public List<Account> getAccounts(User user) {
