59ad46c380d7284247fd9dbd7bab788b613043cd
Bagrat <bagiration@yandex.ru>
2024-10-31 08:28:28 +0300

    3. Отсортировать департаменты [#34352]

---
+++ src/main/java/ru/job4j/collection/Departments.java
@@ -0,0 +1,31 @@
+package ru.job4j.collection;
+
+import java.util.*;
+
+public class Departments {
+    public static List<String> fillGaps(List<String> departments) {
+        Set<String> temp = new LinkedHashSet<>();
+        for (String value : departments) {
+            StringBuilder start = new StringBuilder();
+            for (String element : value.split("/")) {
+                if (start.length() == 0) {
+                    temp.add(element);
+                    start = new StringBuilder(element);
+                } else {
+                    temp.add(start + "/" + element);
+                    start.append("/").append(element);
+                }
+            }
+        }
+        return new ArrayList<>(temp);
+    }
+
+    public static void sortAsc(List<String> departments) {
+        //departments.sort(Comparator.naturalOrder());
+        Collections.sort(departments, Comparator.naturalOrder());
+    }
+
+    public static void sortDesc(List<String> departments) {
+        Collections.sort(departments, new DepartmentsDescComparator());
+    }
+}
---
+++ src/main/java/ru/job4j/collection/DepartmentsDescComparator.java
@@ -0,0 +1,22 @@
+package ru.job4j.collection;
+
+import java.util.Comparator;
+
+public class DepartmentsDescComparator implements Comparator<String> {
+
+    @Override
+    public int compare(String left, String right) {
+        String[] leftArray = left.split("/");
+        String[] rightArray = right.split("/");
+        int compareDepartment = rightArray[0].compareTo(leftArray[0]);
+        if (compareDepartment == 0) {
+            for (int i = 1; i < Math.min(leftArray.length, rightArray.length); i++) {
+                if (leftArray[i].compareTo(rightArray[i]) != 0) {
+                    return leftArray[i].compareTo(rightArray[i]);
+                }
+            }
+            return Integer.compare(leftArray.length, rightArray.length);
+        }
+        return rightArray[0].compareTo(leftArray[0]);
+    }
+}
