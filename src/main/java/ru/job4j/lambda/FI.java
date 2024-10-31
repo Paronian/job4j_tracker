dbe88cf60d8af27f787b34462031d23b1875d16e
Bagrat <bagiration@yandex.ru>
2024-10-31 23:01:40 +0300

    2.0. Лямбда  [#58165]

---
+++ src/main/java/ru/job4j/lambda/FI.java
@@ -0,0 +1,21 @@
+package ru.job4j.lambda;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class FI {
+    public static void main(String[] args) {
+        Attachment[] attachments = {
+                new Attachment("image 1", 20),
+                new Attachment("image 3", 120),
+                new Attachment("image 2", 23)
+        };
+        Comparator<Attachment> comparator = (left, right) -> left.getName().compareTo(right.getName());
+        Comparator<Attachment> comparatorByLengthOfName = (left, right) -> Integer.compare(right.getName().length(), left.getName().length());
+
+        Arrays.sort(attachments, comparator);
+        System.out.println(Arrays.toString(attachments));
+        Arrays.sort(attachments, comparatorByLengthOfName);
+        System.out.println(Arrays.toString(attachments));
+    }
+}
